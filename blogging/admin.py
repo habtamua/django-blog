645fb6fbf6e685eda0b965839de088ed410d7a12
HABTAMU ASFAW <asfawh@uw.edu>
2019-05-20 22:43:57 -0700

    update admin to Include InlineModelAdmin- Categories on the Post adminview

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-
-# Register your models here.
 from blogging.models import Post, Category
 
-# add a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # these two are same
+    # exclude = ('posts',)
+    fields = ('name', 'description')
